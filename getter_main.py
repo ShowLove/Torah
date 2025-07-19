2361c345f983a6949d6d46b6b68fb1399b045e5e
Carlos Garzon <carlosgarzon@aviv.lan>
2025-07-19 17:17:11 -0400

    added a top level main

--- getter_main.py
+++ getter_main.py
@@ -1,15 +1,24 @@
 import sys
 import os
 
-# Add the torah_search_bar folder to the system path
+######################################################################
+# Dependency Folders: torah_search_bar, data, utils ##################
+# torah_search_bar
 current_dir = os.path.dirname(os.path.abspath(__file__))
 torah_search_bar_path = os.path.join(current_dir, "torah_search_bar")
 sys.path.append(torah_search_bar_path)
+# data
+data_path = os.path.join(current_dir, "data")
+sys.path.append(data_path)
+# utils
+utils_path = os.path.join(current_dir, "utils")
+sys.path.append(utils_path)
+######################################################################
 
-# Now imports from submodules should work
 from torah_search_bar import getBookChVerse, getSite
 
 def main():
+
     book, chapter, verse = getBookChVerse.main()
     website = getSite.main()
 
@@ -17,7 +26,7 @@ def main():
     print(f"Book: {book}")
     print(f"Chapter: {chapter}")
     print(f"Verse: {verse}")
-    print(f"Website: {website}")
+    print(f"Eng Website: {website}")
 
 
 if __name__ == "__main__":
